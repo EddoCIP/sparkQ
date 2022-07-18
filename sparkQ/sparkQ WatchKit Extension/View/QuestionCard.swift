0156b0dd99c6567c9a076d0dae5a64aedc05ee79
ditcu-code <63937626+ditcu-code@users.noreply.github.com>
2022-07-18 19:52:44 +0700

    ditcu - align styles, code-splitting and refactor

---
+++ sparkQ/sparkQ WatchKit Extension/View/QuestionCard.swift
@@ -0,0 +1,57 @@
+//
+//  QuestionCard.swift
+//  sparkQ WatchKit Extension
+//
+//  Created by Eddo Careera Iriyanto Putra on 14/07/22.
+//
+
+import SwiftUI
+
+struct QuestionCard: View {
+    var counter: Int
+    var question : QuestionModel
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .center) {
+                Text("\(counter)")
+                    .bold()
+                Spacer()
+                Group {
+                    if question.isChecked {
+                        Image(systemName: "checkmark.circle.fill")
+                            .foregroundColor(Color.white)
+                    }
+                    if question.isPinned {
+                        Image(systemName: "pin.circle.fill")
+                            .foregroundColor(Color.white)
+                            .rotationEffect(Angle(degrees: 45))
+                    }
+                }
+                .opacity(0.7)
+            }
+            .padding(.vertical, 6)
+            .padding(.leading, 5)
+            .font(.title3)
+            Text(question.question)
+                .font(.footnote)
+                .padding(.horizontal, 5)
+        }
+        .padding(.bottom, 10)
+        .listRowBackground(
+            LinearGradient(
+                gradient:Gradient(colors: question.isPinned ? [.orange, .yellow] : [.cyan, .blue]),
+                startPoint: .topTrailing,
+                endPoint: .bottomLeading
+            )
+            .opacity(0.4)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+        )
+    }
+}
+
+struct QuestionCard_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionCard(counter: 0, question: QuestionModel())
+    }
+}
